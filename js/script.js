79fe2158fdd9c57a014d3a6810392af76a8c3cf1
Maxime TYTGAT <maxime2103i@gmail.com>
2020-11-05 20:14:55 +0100

    feat(view): initialise view

---
+++ js/script.js
@@ -0,0 +1,9 @@
+let serch_bar = document.querySelector('.wrap') // Serch barS
+let input = document.querySelector('#search_input') // Input
+
+input.addEventListener('input', () => {
+        
+    console.log("la");
+})
+
+

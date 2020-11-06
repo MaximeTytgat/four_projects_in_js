3138986afaba437804a6c189296c7a12899e48eb
Maxime TYTGAT <maxime2103i@gmail.com>
2020-11-06 20:04:46 +0100

    add script api request

--- js/script.js
+++ js/script.js
@@ -1,9 +1,22 @@
 let serch_bar = document.querySelector('.wrap') // Serch barS
 let input = document.querySelector('#search_input') // Input
 
-input.addEventListener('input', () => {
-        
-    console.log("la");
+
+input.addEventListener('input', async event => {
+
+    let search = input.value
+
+    const endpoint = `https://en.wikipedia.org/w/api.php?action=opensearch&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=20&search=${search}`;
+
+
+    fetch(endpoint).then(async res => {
+        let non = await res.json()
+        console.log(non);
+    }).catch(error => {
+        console.error(error)
+    })
+
+
 })
 
 

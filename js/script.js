04af30d7d461a5332b9ec1012cf492ec2097fd28
maxime2103i <48186655+maxime2103i@users.noreply.github.com>
2020-11-07 14:32:53 +0100

    feat(search): fix order of search

--- js/script.js
+++ js/script.js
@@ -12,9 +12,6 @@ const create_element = (tag, className) => {
 }
 
 
-
-
-
 const create_result_block = (title, url, desc) => {
     let result_container = create_element('div', 'result_container');
     let juigoogleresult = create_element('div', 'jui-google-result');
@@ -26,14 +23,30 @@ const create_result_block = (title, url, desc) => {
     let result__description = create_element('div', 'jui-google-result__description');
     result__description.innerHTML ='<a href="' + url + '" target="_blank">' +  desc + '</a><span class="icon-edit">';
 
+    
     juigoogleresult.appendChild(result__headline);
     juigoogleresult.appendChild(result__link);
     juigoogleresult.appendChild(result__description);
     result_container.appendChild(juigoogleresult);
-    search_result.appendChild(result_container);
+    return result_container; 
 }
 
 
+const get_desc = async search => {
+    const endpoint = `https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&origin=*&exintro&explaintext&redirects=1&titles=${search}`;;
+    
+    let request = await fetch(endpoint);
+    if (request.ok) {
+        let json = await request.json();
+        let desc = json.query.pages[Object.keys(json.query.pages)[0]].extract.split('. ');
+        let desc_reduce = desc[0] + desc[1];
+        return desc_reduce;
+    }
+}
+
+
+
+let nb = 0;
 
 input.addEventListener('input', async () => {
 
@@ -42,19 +55,21 @@ input.addEventListener('input', async () => {
 
     fetch(endpoint).then(async res => {
         let result = await res.json()
+        
+        number_of_search = "search_number_" + nb;
+        let search_number = create_element('div', number_of_search);
+        nb++;
+        
+        search_result.removeChild(search_result.firstChild);
 
-        while (search_result.firstChild) {
-            search_result.removeChild(search_result.firstChild);
-        }
+        console.log(result);
+
+
+        search_result.appendChild(search_number)
 
         for (const i in result[1]) {
-            const endpoint = `https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&origin=*&exintro&explaintext&redirects=1&titles=${result[1][i]}`;
-            fetch(endpoint).then(async res => {
-                result_desk = await res.json()
-                let desc = result_desk.query.pages[Object.keys(result_desk.query.pages)[0]].extract.split('. ');
-                let desc_reduce = desc[0] + desc[1]
-                create_result_block(result[1][i], result[3][i], desc_reduce);
-            })
+            let desc = await get_desc(result[1][i]);
+            search_number.appendChild(create_result_block(result[1][i], result[3][i], desc));
         }
 
     }).catch(error => {
